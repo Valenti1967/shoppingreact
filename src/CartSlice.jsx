90623acf165f004333c8f927341e5ecbe989cee2
Valenti1967 <valenti.vlaev@gmail.com>
2025-03-02 01:47:51 -0500

    my changes

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,19 +4,43 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    countCartItems: 0,
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity++;
+        } else {
+          state.items.push({ name, image, cost, quantity: 1 });
+        }
+        state.countCartItems++;
     },
     removeItem: (state, action) => {
+        const { name, quantity } = action.payload;
+        state.items = state.items.filter(item => item.name !== name);
+        if(quantity > 0){
+            state.countCartItems -= quantity; 
+        }
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+            if(quantity > itemToUpdate.quantity){
+                state.countCartItems += (quantity - itemToUpdate.quantity); 
+            } else if(quantity < itemToUpdate.quantity) {
+                state.countCartItems -= (itemToUpdate.quantity - quantity);
+            }
+            itemToUpdate.quantity = quantity;
+        }        
     },
   },
 });
+
+export const selectCartItems = (state) => state.cart.items;
+export const selectCountCartItems = (state) => state.cart.countCartItems;
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,20 @@
 import React, { useState,useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice'
+import { selectCartItems, selectCountCartItems } from './CartSlice';
+
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page  
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const dispatch = useDispatch();
+    const cartItemCount = useSelector(selectCountCartItems);
+    const cartItems = useSelector(selectCartItems);
+
 
     const plantsArray = [
         {
@@ -232,6 +243,16 @@ function ProductList() {
     fontSize: '30px',
     textDecoration: 'none',
    }
+
+   const handleAddToCart = (product) => {
+    console.log(product);
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+       ...prevState,
+       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+     }));
+  };
+
    const handleCartClick = (e) => {
     e.preventDefault();
     setShowCart(true); // Set showCart to true when cart icon is clicked
@@ -263,12 +284,29 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>{cartItemCount > 0 && <span className="cart_quantity_count">{cartItemCount}</span>}</h1></a></div>
             </div>
         </div>
         {!showCart? (
         <div className="product-grid">
-
+                {plantsArray.map((category, index) => (
+                    <div key={index}>
+                        <h1><div className="category-title">{category.category}</div></h1>
+                        <div className="product-list">
+                            {category.plants.map((plant, plantIndex) => (
+                            <div className="product-card" key={plantIndex}>
+                                <img className="product-image" src={plant.image} alt={plant.name} />
+                                <div className="product-title">{plant.name}</div>
+                                <div className="product-title">{plant.description}</div>
+                                <div className="product-title">{plant.cost}</div>
+                                {/*Similarly like the above plant.name show other details like description and cost*/}
+                                <button className={cartItems.find(item => item.name === plant.name) ?  "product-button btn-disabled" : "product-button"} 
+                                onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                            </div>
+                            ))}
+                        </div>
+                    </div>
+                    ))}
 
         </div>
  ) :  (
